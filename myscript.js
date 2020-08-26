e368893e733be4286a5eff1ee9998bd4b4261134
kevin rios <kevinsitho7@gmail.com>
2020-08-26 00:49:24 -0500

    publicacion de feliz cumpleaños

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function proceed(){
+    document.getElementById("loaded").style="display:none"
+    var obj=document.getElementById("a");
+        obj.play();
+
+}
+alert("Sube el volumen cielo 🔊");
+alert("Feliz Cumpleaños Mi amor");
+//alert("Try to click on the pic")
